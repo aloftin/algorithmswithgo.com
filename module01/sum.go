f9e448fca0b31b2337e7f1c9689392227c952a16
Adam Loftin <aloftin@rxbenefits.com>
2021-01-28 08:18:02 -0600

    Solved Sum

--- module01/sum.go
+++ module01/sum.go
@@ -2,6 +2,21 @@ package module01
 
 // Sum will sum up all of the numbers passed
 // in and return the result
+
+// Original solution
+// func Sum(numbers []int) int {
+// 	var sum int
+// 	for _, val := range numbers {
+// 		sum += val
+// 	}
+
+// 	return sum
+// }
+
+// Recursion solution
 func Sum(numbers []int) int {
-	return 0
+	if len(numbers) == 0 {
+		return 0
+	}
+	return numbers[0] + Sum(numbers[1:])
 }
