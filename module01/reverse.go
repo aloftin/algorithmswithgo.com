3a620c26dc06a52ffe7ad9b0aa63a39d68812c6c
Adam Loftin <aloftin@rxbenefits.com>
2021-01-29 14:17:48 -0600

    Solved Reverse

--- module01/reverse.go
+++ module01/reverse.go
@@ -6,6 +6,36 @@ package module01
 //   Reverse("cat") => "tac"
 //   Reverse("alphabet") => "tebahpla"
 //
+
+// Looping in reverse
+// func Reverse(word string) string {
+// 	var res string
+
+// 	for i := len(word) - 1; i >= 0; i-- {
+// 		res = res + string(word[i])
+// 	}
+
+// 	return res
+// }
+
+// Looping in reverse with strings.Builder
+// func Reverse(word string) string {
+// 	var sb strings.Builder
+
+// 	for i := len(word) - 1; i >= 0; i-- {
+// 		sb.WriteByte(word[i])
+// 	}
+
+// 	return sb.String()
+// }
+
+// Looping normally with range and runes - handles multi-byte chars in the word
 func Reverse(word string) string {
-	return ""
+	var res string
+
+	for _, r := range word {
+		res = string(r) + res
+	}
+
+	return res
 }
